ac83089d2bf9721016d51aae93989c7ce83c8660
phantomgoose <alex.txt@gmail.com>
2017-08-03 12:50:31 -0700

    start adding logic to Hamilton game, add player.gethand method

--- hamilton.py
+++ hamilton.py
@@ -1,14 +1,34 @@
 '''
 Game logic:
-    1) Generate a deck
-    2) Shuffle the deck
-    3) Create 2 players
-    4) Each player draws a card
+    1) Generate a deck - done
+    2) Shuffle the deck - done on deck generation
+    3) Create 2 players - done
+    4) Each player draws a card - done
     5) Whoever's card is better than the other's gets a point
     6) Once the deck is empty, the player with the most points wins
 '''
 
 from deck import Deck
+from player import Player
 
+#generate game objects
 d = Deck()
-d.show()
+p1 = Player("Alex")
+p2 = Player("Chris")
+
+test_count = 1
+
+#while there are cards remaining in the deck
+while (len(d.cards) > 0 and test_count > 0):
+    #both players draw
+    p1.draw(d)
+    p2.draw(d)
+
+    #compare draws
+
+    #print first card in the hand for each player
+    print p1.getHand(1)[0].show()
+    print p2.getHand(1)[0].show()
+
+    #make sure we dont enter an infinite loop
+    test_count -= 1
--- player.py
+++ player.py
@@ -2,6 +2,7 @@ class Player(object):
     def __init__(self,name):
         self.name = name
         self.hand = []
+        self.points = 0
 
     #draws a card from deck.cards
     def draw(self, deck):
@@ -13,6 +14,20 @@ class Player(object):
         for card in self.hand:
             card.show()
 
+    #returns x cards from the hand, default all
+    def getHand(self, num=None):
+        res = []
+        #make sure we dont go out of range and also set a default num
+        if num > len(self.hand) or num == None:
+            num = len(self.hand)
+        for c in range(num):
+            res.append(self.hand[c])
+        return res
+
     #discards the last card in the hand
     def discard(self):
         return self.hand.pop()
+
+    #add point(s), 1 point by default
+    def addPts(self, pts=1):
+        self.points += pts
