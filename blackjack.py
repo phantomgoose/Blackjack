67966a043357901ebfc0b470399807ee155400e4
Foxc6 <foxc6@hotmail.com>
2017-08-03 13:41:09 -0700

    Adds blackjack.py file with game outline

---
+++ blackjack.py
@@ -0,0 +1,29 @@
+""" 
+#####MVP#####
+###GAME SETUP###
+
+Create deck - done
+Shuffle deck - done
+Create 2-6 players
+Each play is dealt two cards
+
+
+
+###GAMEPLAY###
+Set value of fade cards to 10
+Set initial value of Ace Cards to 11
+If total hand value exceeds 21, Ace value is changed to 1
+First player decides to get another card or pass
+(continue until all players have made this decidion)
+
+Dealer must keep drawing new card until total hand value reaches minimum of 17
+Once dealer reaches 17-21 game ends
+Each player that hasn't exceeded 21 and has a higher hand value than the dealer, wins
+
+
+
+#####ADD ONS#####
+Add ability to bet poker chips
+Add card counter, in order to make the Dealer more competitive
+Add GUI, rather than running in console
+ """
