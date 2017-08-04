7bf5f71f39e1115e5767822cca38c183f122e68f
phantomgoose <alex.txt@gmail.com>
2017-08-03 18:37:32 -0700

    trim unused code in blackjack

--- blackjack.py
+++ blackjack.py
@@ -1,33 +1,3 @@
-"""
-#####MVP#####
-###GAME SETUP###
-
-Create deck - done
-Shuffle deck - done on deck creation
-Create 1 player and a dealer
-Each play is dealt two cards
-
-
-
-###GAMEPLAY###
-Set value of fade cards to 10
-Set initial value of Ace Cards to 11
-If total hand value exceeds 21, Ace value is changed to 1
-Player decides to get another card or pass
-
-Dealer must keep drawing new card until total hand value reaches minimum of 17
-Once dealer reaches 17-21 game ends
-If player hasn't exceeded 21 and has a higher hand value than the dealer, the player wins
-
-
-
-#####ADD ONS#####
-Add ability to bet poker chips
-Add card counter, in order to make the Dealer more competitive
-Add GUI, rather than running in console
-Add ability for the player to choose their name
- """
-
 from deck import Deck
 from player import Player
 
@@ -71,19 +41,6 @@ def isGameOver(p, d):
     if (p.points >= 21 or d.points >= 21):
         return True
     return False
-
-def getPlayerChoice():
-    print "****************************************************************************************"
-    player_choice = raw_input("HIT OR STAND? ")
-    if player_choice != "hit" and player_choice != "stand":
-        print "Invalid input, try again"
-        getPlayerChoice()
-    else:
-        return player_choice
-
-def showGameState():
-    print "You have " + str(player.points) + " points. Your hand consists of: ", player.showHand()
-    print "Dealer has " + str(dealer.points) + " points. Their hand consists of: ", dealer.showHand()
 
 #calculate starting points for both player and dealer
 calcPoints(player)
@@ -171,18 +128,3 @@ def play(choice):
         print "Game over"
         print getDealerCards()
         print getDealerPoints()
-'''
-    #after the game ends
-    print "***************GAME OVER***************"
-
-    if player.points > 21 and dealer.points > 21:
-        #REFACTOR THIS MESS
-        print "NOBODY WINS!"
-        showGameState()
-    elif (player.points > dealer.points or dealer.points > 21) and player.points <= 21:
-        print "YOU WIN!"
-        showGameState()
-    else:
-        print "YOU LOSE!"
-        showGameState()
-'''
